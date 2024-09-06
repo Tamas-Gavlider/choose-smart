8dbc723051cd9f03eb47d38bdb6eb3b280067eab
Tamas Gavlider <6mv8g9c8@students.codeinstitute.net>
2024-09-06 11:56:13 +0000

    Fixed a console error in start js by adding preventDefault to event

--- assets/js/start.js
+++ assets/js/start.js
@@ -1,19 +1,17 @@
 // Get user name and age and submit details to start the game
 
-function getStarted() {
+function getStarted(event) {
+    event.preventDefault();
     let name = document.getElementById('name').value;
     // First character will be capitalized
     let capitalizedName = name.charAt(0).toUpperCase() + name.slice(1);
     let age = document.getElementById('age').value;
     let div = document.getElementById('player-details');
+    // Insert the welcome message with the name/age values and link to instructions/start game
     div.innerHTML = `<div id='player-details'><p>Hello ${capitalizedName}! You had ${age} years to learn that all your actions have consequences!</p>
       
       <a href='instructions.html' class='nav-btn hover-effects'>How to play?</a>
       <a href='game.html' class='nav-btn hover-effects' id='start-game'>Start Game</a></div>`;
-    // Keep the feedback arrows at the same position after user submit name and age
-    let feedback = document.getElementById('feedback');
-    feedback.style.marginTop = '105px';
-  
   }
 
   let form = document.getElementById('entry');
